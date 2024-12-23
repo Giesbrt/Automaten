d9a0d7fa3952e4b25a379ef4dc3c80f289823774
Giesbrt <131985085+Giesbrt@users.noreply.github.com>
2024-12-23 16:36:20 +0100

    added wip simulationLoader with automatonProvider

---
+++ src/default-config/core/modules/automaton/automatonProvider.py
@@ -0,0 +1,49 @@
+"""TBA"""
+
+# Abstract Machine related imports
+from core.modules.automaton.dfa import DFAAutomaton, DFAState, DFATransition
+from core.modules.automaton.TM import TMAutomaton, TMState, TMTransition
+
+from aplustools.io import ActLogger
+
+# Standard typing imports for aps
+import collections.abc as _a
+import abc as _abc
+import typing as _ty
+import types as _ts
+
+
+class AutomatonProvider:
+    registered_automatons: _ty.Dict[str, _ty.Dict[str, _ty.Callable]] = {}
+
+    def __init__(self, automaton_type: str) -> None:
+        self.automaton_type: str = automaton_type
+
+        # registered Automatons
+        self.register_automaton('dfa', DFAAutomaton.BaseAutomaton, DFAState.BaseState, DFATransition.BaseTransition)
+        self.register_automaton('tm', TMAutomaton.BaseAutomaton, TMState.BaseState, TMTransition.BaseTransition)
+
+    def set_automaton_type(self, new_type: str) -> None:
+        self.automaton_type = new_type
+
+    def get_automaton_type(self) -> str:
+        return self.automaton_type
+
+    def register_automaton(self, name: str, base: _ty.Callable, state: _ty.Callable, transition: _ty.Callable) -> None:
+        if name.lower() in self.registered_automatons:
+            return
+
+        automaton_data: _ty.Dict[str, _ty.Callable] = {'base': base,
+                                                       'state': state,
+                                                       'transition': transition}
+        self.registered_automatons[name.lower()] = automaton_data
+        ActLogger().info(f"Registered new {name.lower()}-Automaton")
+
+    def get_automaton_base(self) -> _ty.Callable:
+        return self.registered_automatons[self.automaton_type]['base']
+
+    def get_automaton_state(self) -> _ty.Callable:
+        return self.registered_automatons[self.automaton_type]['state']
+
+    def get_automaton_transition(self) -> _ty.Callable:
+        return self.registered_automatons[self.automaton_type]['transition']
---
+++ src/default-config/core/modules/automaton/simulationLoader.py
@@ -0,0 +1,123 @@
+"""TBA"""
+from returns import result as _result
+
+# Abstract Machine related imports
+from core.modules.automaton.automatonBridge import AutomatonBridge
+from core.modules.automaton.base.automaton import Automaton as BaseAutomaton
+from automatonProvider import AutomatonProvider
+
+from queue import Queue
+
+from aplustools.io import ActLogger
+from serializer import Serializer
+from core.modules.storage import JSONAppStorage
+
+# Standard typing imports for aps
+import collections.abc as _a
+import abc as _abc
+import typing as _ty
+import types as _ts
+
+
+class SimulationLoader:
+
+    def __init__(self, json_app_storage: JSONAppStorage):
+        super().__init__()
+        # App storage access
+        self.app_storage: JSONAppStorage = json_app_storage
+        loaded_max_restart_counter: int = self.app_storage.retrieve("simulation_loader_max_restart_counter", int)
+
+        self.restart_counter: int = 0
+        self.max_restart_counter: int = loaded_max_restart_counter or 5  # set default of 5 if the config could not be read
+
+        self.error_callback: _ty.Callable or None = None
+        self.error_cache: Queue[str] = Queue()
+
+        self.automaton: AutomatonBridge = None
+
+    def set_error_callback(self, callback: _ty.Callable) -> None:
+        self.error_callback = callback
+
+    def get_error_callback(self) -> _ty.Callable or None:
+        return self.error_callback
+
+    def save_automaton(self, file_path: str) -> _result.Result:
+        automaton_implementation: BaseAutomaton = self.automaton.get_implementation()
+        serialisation_result: _result.Result = Serializer.serialise(automaton_implementation, file_path)
+
+        return serialisation_result
+
+    def load_automaton(self, file_path: str) -> _result.Result:
+        deserialization_result: _result.Result = Serializer.load(file_path)
+        if not isinstance(deserialization_result, _result.Success):
+            return deserialization_result
+
+        loaded_automaton = deserialization_result.value_or(None)
+        if loaded_automaton is None:
+            log_message: str = "Could not load automaton: Automaton is None"
+            ActLogger().error(log_message)
+            return _result.Failure(log_message)
+
+        if not isinstance(loaded_automaton, BaseAutomaton):
+            log_message: str = f"Loaded automaton is not recognised as an automaton: {type(loaded_automaton).__name__}"
+            ActLogger().error(log_message)
+            return _result.Failure(log_message)
+
+        automaton_bridge: AutomatonBridge = AutomatonBridge(loaded_automaton)
+        self.automaton = automaton_bridge
+        self.__reset_temp_data()
+
+        log_message: str = f"Loaded automaton of type {type(loaded_automaton).__name__}"
+        ActLogger().info(log_message)
+        return _result.Success(log_message)
+
+    def init_automaton(self, automaton_type: str) -> None:
+        automaton_provider: AutomatonProvider = AutomatonProvider(automaton_type)
+        automaton_implementation: BaseAutomaton = automaton_provider.get_automaton_base()()
+        self.automaton = AutomatonBridge(automaton_implementation)
+        ActLogger().info(f"Initialised new {automaton_type}-Automaton")
+
+        self.__reset_temp_data()
+
+    def __reset_temp_data(self) -> None:
+        self.restart_counter = 0
+        self.error_cache = Queue()
+
+    def serialise_automaton_to_bridge(self) -> None:  # Todo serialise the automaton and push to bridge
+        pass
+
+    def handle_bridge(self) -> None:  # Todo get data from bridge and handle it
+        pass
+
+    def simulate(self) -> _result.Result:
+        try:
+            return_result = self.automaton.simulate_one_step()
+            self.serialise_automaton_to_bridge()
+
+            while return_result is None:
+                return_result = self.automaton.simulate_one_step()
+                self.serialise_automaton_to_bridge()
+
+            # Todo send simulation result to user
+
+        except Exception as e:
+            log_message: str = (f"An error occurred whilst trying to simulate an "
+                                f"{self.automaton.get_implementation_name()}: {str(e)}")
+            self.__handle_failure(log_message, self.simulate)
+            return _result.Failure(log_message)
+
+    def get_restart_counter(self) -> int:
+        return self.restart_counter
+
+    def __handle_failure(self, log_message: str, action: _ty.Callable = None) -> None:
+        self.restart_counter += 1
+        ActLogger().error(log_message)
+        self.error_cache.put(log_message)
+
+        if self.restart_counter < self.max_restart_counter:
+            if action is not None:
+                action()
+            return
+
+        # notify the user that the module is broken
+        self.error_callback()

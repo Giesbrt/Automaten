9fc8ca08f6f795ba198b081e02f1afdd82928bf4
Giesbrt <131985085+Giesbrt@users.noreply.github.com>
2025-02-10 20:04:13 +0100

    added override parameter

--- src/default-config/core/modules/automaton/automatonProvider.py
+++ src/default-config/core/modules/automaton/automatonProvider.py
@@ -28,16 +28,17 @@ class AutomatonProvider:
         self.register_automaton('tm', TMAutomaton.TMAutomaton, TMState.TMState, TMTransition.TMTransition)
         self.register_automaton("mealy", mealyAutomaton.MealyAutomaton, mealyState.MealyState, mealyTransition.MealyTransition)
 
-    def load_from_dict(self, loaded_automatons: _ty.Dict[str, _ty.List[_ty.Callable]]) -> None:
+    def load_from_dict(self, loaded_automatons: _ty.Dict[str, _ty.List[_ty.Callable]], override: bool = False) -> None:
         """ Loads the required automatons classes from a dictionary
 
         :param loaded_automatons: {'Automaton': [Automaton, State, Transition]}
+        :param override: Should the previous registration be overruled
         :return: None
         """
         # 0: base; 1: State; 2: Transition
         for key in list(loaded_automatons.keys()):
             data = loaded_automatons[key]
-            self.register_automaton(key, *data)
+            self.register_automaton(key, data[0], data[1], data[2], override)
 
     def set_automaton_type(self, new_type: str) -> None:
         """Set the type of the automaton
@@ -54,16 +55,17 @@ class AutomatonProvider:
         """
         return self.automaton_type
 
-    def register_automaton(self, name: str, base: _ty.Callable, state: _ty.Callable, transition: _ty.Callable) -> None:
+    def register_automaton(self, name: str, base: _ty.Callable, state: _ty.Callable, transition: _ty.Callable, override: bool = False) -> None:
         """Register a new automaton
         
         :param name: The name of the automaton
         :param base: The base class of the automaton
         :param state: The state class of the automaton
         :param transition: The transition class of the automaton
+        :param override: Should the previous registration be overruled
         :return: None
         """
-        if name.lower() in self.registered_automatons:
+        if name.lower() in self.registered_automatons and not override:
             return
 
         automaton_data: _ty.Dict[str, _ty.Callable] = {'base': base,

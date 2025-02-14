e0119c7eea405ad7c9ad05eb7a879ee46eafe0f6
Giesbrt <131985085+Giesbrt@users.noreply.github.com>
2025-02-14 20:56:14 +0100

    updated serialiserTest.py

--- src/default-config/core/modules/automaton/logic_tests/serialiserTest.py
+++ src/default-config/core/modules/automaton/logic_tests/serialiserTest.py
@@ -5,6 +5,10 @@ from pprint import pprint
 states_with_design: dict = {"end": "Circle: ((180.0, 180.0), 162.0), 2##000000;",
                                 "default": ""}
 
+tokens: list[str] = ["a", "b"]
+changeable_token_list: list[bool] = [True]
+transition_pattern: list[int] = [0]
+
 uiAutomaton = UiAutomaton("dfa", "Giesbrt", states_with_design)
 # States
 stateA = UiState(None, None, "q0", "default")
@@ -13,15 +17,19 @@ stateB = UiState(None, None, "q1", "end")
 uiAutomaton.add_state(stateA)
 uiAutomaton.add_state(stateB)
 # Transitions
-transitionAA = UiTransition(stateA, "n", stateA, "n", ["b", ])
-transitionAB = UiTransition(stateA, "n", stateB, "n", ["a", ])
-transitionBA = UiTransition(stateB, "n", stateA, "n", ["b", ])
-transitionBB = UiTransition(stateB, "n", stateB, "n", ["a", ])
+transitionAA = UiTransition(stateA, "n", stateA, "n", [tokens[1], ])
+transitionAB = UiTransition(stateA, "n", stateB, "n", [tokens[0], ])
+transitionBA = UiTransition(stateB, "n", stateA, "n", [tokens[1], ])
+transitionBB = UiTransition(stateB, "n", stateB, "n", [tokens[0], ])
 
 uiAutomaton.add_transition(transitionAB)
 uiAutomaton.add_transition(transitionAA)
 uiAutomaton.add_transition(transitionBA)
 uiAutomaton.add_transition(transitionBB)
+
+uiAutomaton.set_token_lists([tokens])
+uiAutomaton.set_is_changeable_token_list(changeable_token_list)
+uiAutomaton.set_transition_pattern(transition_pattern)
 
 serialised = serialize(uiAutomaton)
 pprint(serialised)

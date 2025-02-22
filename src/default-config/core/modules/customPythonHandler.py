4e486adc3d236b80b6fd3260ce06011cde61a4f1
Giesbrt <131985085+Giesbrt@users.noreply.github.com>
2025-02-22 17:20:46 +0100

    added customPythonHandler

---
+++ src/default-config/core/modules/customPythonHandler.py
@@ -0,0 +1,53 @@
+from returns import result as _result
+from os import path
+from aplustools.io.fileio import os_open
+from utils.errorCache import ErrorCache, ErrorSeverity
+from traceback import format_exc
+
+# Standard typing imports for aps
+import collections.abc as _a
+import abc as _abc
+import typing as _ty
+import types as _ts
+
+
+class CustomPythonHandler:
+
+    def __init__(self) -> None:
+        super().__init__()
+        # use os open (aplustools)
+
+    def load(self, custom_python: str, automaton_type: str, extensions_path: str) -> _result.Result:
+        if not custom_python or not automaton_type:
+            return _result.Failure("Attributes can not be None or empty!")
+
+        if path.exists(extensions_path):
+            # file exists -> SKIP
+            return _result.Failure("The file already exists!")
+
+        try:
+            with os_open(extensions_path, "wb") as f:
+                f.write(custom_python.encode())
+        except Exception as e:
+            ErrorCache().error("An error occurred whilst trying to load a custom python extension",
+                               format_exc(), True, ErrorSeverity.FATAL)
+            return _result.Failure(f"An error occurred whilst trying to load a custom python extension {str(e)}!")
+        return _result.Success("File successfully created!")
+
+    def to_custom_python(self, module_path: str) -> str:
+        if not module_path:
+            return ""
+
+        try:
+            with os_open(module_path, "rb") as f:
+                file_content: bytes = f.read()
+
+        except Exception as e:
+            ErrorCache().error("An error occurred whilst trying to load a custom python extension",
+                               format_exc(), True)
+            return ""
+
+        if not file_content:
+            return ""
+
+        return file_content.decode()
